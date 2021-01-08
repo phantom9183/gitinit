9e0f722f316c947a21af71450c88258f7ec611f1
test <test@test.co.kr>
2021-01-07 21:26:16 -0500

    Commit This Branch

--- hello.c
+++ hello.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "hello.h"
 
 int main(int argc, char **argv){
 	printf("Hello World!\n");
@@ -32,5 +33,8 @@ int main(int argc, char **argv){
 	printf("Hello Hello!\n");
 	printf("Hello Hello!\n");
 	printf("Hello Hello!\n");
+
+
+	printf("%d\n",sum(10,20));
 	return 0;
 }
