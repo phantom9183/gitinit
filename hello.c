2d67605a55b6bdad9773898615040855049bd688
test <test@test.co.kr>
2021-01-08 17:21:54 +0900

    full commit push

--- hello.c
+++ hello.c
@@ -36,11 +36,9 @@ int main(int argc, char **argv){
 
 
 	printf("%d\n",sum(10,20));
-<<<<<<< HEAD
 	printf("%d\n",minus(30,20));
-=======
 	printf("precommit\n");
 	printf("precommit_fix_now\n");
->>>>>>> refs/heads/precommit
+
 	return 0;
 }
